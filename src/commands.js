cb87cae4ecfa506fecd121508fee14ec56142fbb
crowterliam <liamtdcrowter@gmail.com>
2025-03-05 16:18:16 +0000

    refactor: rename project from 'awwbot' to 'diskingdom' and remove unused commands

--- src/commands.js
+++ src/commands.js
@@ -6,20 +6,8 @@
 import { WARFARE_COMMANDS } from './knw/commands/warfare.js';
 import { INTRIGUE_COMMANDS } from './knw/commands/intrigue.js';
 
-export const AWW_COMMAND = {
-  name: 'awwww',
-  description: 'Drop some cuteness on this channel.',
-};
-
-export const INVITE_COMMAND = {
-  name: 'invite',
-  description: 'Get an invite link to add the bot to your server',
-};
-
 // Export all commands
 export const ALL_COMMANDS = [
-  AWW_COMMAND,
-  INVITE_COMMAND,
   ...WARFARE_COMMANDS,
   ...INTRIGUE_COMMANDS,
 ];
